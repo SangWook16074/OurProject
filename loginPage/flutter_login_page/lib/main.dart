a4ed6680b1017ca1f46f7938bbe2ee64410c1443
HanSangWook <qazws78941@naver.com>
2022-06-29 02:37:37 +0900

    version 8

--- loginPage/flutter_login_page/lib/main.dart
+++ loginPage/flutter_login_page/lib/main.dart
@@ -51,7 +51,7 @@ class MyHomePage extends StatelessWidget {
                         color: Colors.white),
                   ),
                   Text(
-                    "정보통신공학과 학생회",
+                    "정보통신공학과",
                     style: TextStyle(
                         fontSize: 40,
                         fontStyle: FontStyle.normal,
@@ -65,6 +65,31 @@ class MyHomePage extends StatelessWidget {
                     decoration: InputDecoration(
                         border: OutlineInputBorder(),
                         labelText: "비밀번호를 입력하세요."),
+                  ),
+                  ElevatedButton(
+                    //로그인 버튼
+                    onPressed: () {
+                      //로그인 메소드
+                    },
+                    child: Text(
+                      "로그인",
+                      style: TextStyle(fontSize: 15, letterSpacing: 4.0),
+                    ),
+                    style: TextButton.styleFrom(
+                        backgroundColor: Colors.red[400],
+                        padding: EdgeInsets.all(16.0),
+                        minimumSize: Size(400, 25)),
+                  ),
+                  TextButton(
+                    //회원가입 버튼
+                    onPressed: () {
+                      //회원가입 메소드
+                    },
+                    child: Text(
+                      "회원가입",
+                      style: TextStyle(fontSize: 15, color: Colors.red[400]),
+                    ),
+                    style: TextButton.styleFrom(),
                   )
                 ],
               )
