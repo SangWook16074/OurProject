600410ddceb9e0803c386b026bcdb1b39a6273fb
HanSangWook <qazws78941@naver.com>
2022-06-30 03:13:03 +0900

    version 3

--- flutter_main_page/lib/main_page_sub/main_home.dart
+++ flutter_main_page/lib/main_page_sub/main_home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class MainPage1 extends StatelessWidget {
   const MainPage1({Key? key}) : super(key: key);
@@ -13,4 +14,28 @@ class MainPage1 extends StatelessWidget {
       )),
     );
   }
+}
+
+Widget _buildTop() {
+  return CarouselSlider(
+    options: CarouselOptions(height: 400.0),
+    items: [1, 2, 3, 4, 5].map((i) {
+      return Builder(
+        builder: (BuildContext context) {
+          return Container(
+              width: MediaQuery.of(context).size.width,
+              margin: EdgeInsets.symmetric(horizontal: 5.0),
+              decoration: BoxDecoration(color: Colors.amber),
+              child: Text(
+                'text $i',
+                style: TextStyle(fontSize: 16.0),
+              ));
+        },
+      );
+    }).toList(),
+  );
+}
+
+Widget _buildBottom() {
+  return Text("Top");
 }
