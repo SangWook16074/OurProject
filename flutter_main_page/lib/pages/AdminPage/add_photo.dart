4007724de1a9dd50d67b3003bf439d81209befca
HanSangWook <qazws78941@naver.com>
2022-07-24 13:33:33 +0900

    안드로이드 완성

--- flutter_main_page/lib/pages/AdminPage/add_photo.dart
+++ flutter_main_page/lib/pages/AdminPage/add_photo.dart
@@ -1,2 +1,152 @@
+import 'dart:io';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
+const Duration _duration = Duration(milliseconds: 300);
+
+class AddPhotoPage extends StatefulWidget {
+  const AddPhotoPage({Key? key}) : super(key: key);
+
+  @override
+  State<AddPhotoPage> createState() => _AddPhotoPageState();
+}
+
+class _AddPhotoPageState extends State<AddPhotoPage> {
+  PlatformFile? pickedFile;
+  UploadTask? uploadTask;
+  var _picked = false;
+  var _check = false;
+
+  Future selectFile() async {
+    final result = await FilePicker.platform.pickFiles();
+    if (result == null) return;
+
+    setState(() {
+      pickedFile = result.files.first;
+      _picked = true;
+    });
+  }
+
+  Future uploadFile() async {
+    final path = 'files/${pickedFile!.name}';
+    final file = File(pickedFile!.path!);
+
+    final ref = FirebaseStorage.instance.ref().child(path);
+
+    setState(() {
+      uploadTask = ref.putFile(file);
+      _check = true;
+    });
+
+    final snapshot = await uploadTask!.whenComplete(() {});
+
+    final title = pickedFile!.name;
+    final urlDonwload = await snapshot.ref.getDownloadURL();
+    FirebaseFirestore.instance.collection('eventImage').doc().set({
+      'url': urlDonwload,
+      'title': title,
+    });
+
+    setState(() {
+      uploadTask = null;
+    });
+
+    Navigator.of(context).pop();
+    Fluttertoast.showToast(
+        msg: "완료 !",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        fontSize: 16);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: const Text(
+          "ADD PHOTO",
+          style: TextStyle(
+            color: Colors.white,
+            fontFamily: 'Pacifico',
+          ),
+        ),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: selectFile,
+            icon: Icon(Icons.add_a_photo),
+          ),
+          IconButton(onPressed: uploadFile, icon: Icon(Icons.check))
+        ],
+      ),
+      body: ListView(
+        children: [
+          if (pickedFile != null)
+            Padding(
+              padding: const EdgeInsets.all(40.0),
+              child: Column(
+                children: [
+                  Container(
+                    child: Image.file(
+                      File(pickedFile!.path!),
+                      width: double.infinity,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  AnimatedOpacity(
+                      duration: _duration,
+                      opacity: _check ? 0.0 : 1.0,
+                      child: ElevatedButton(
+                          onPressed: () {
+                            setState(() {
+                              pickedFile = null;
+                            });
+                          },
+                          child: Text("취소"))),
+                ],
+              ),
+            ),
+          _buildProgress(),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildProgress() => StreamBuilder<TaskSnapshot>(
+      stream: uploadTask?.snapshotEvents,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          final data = snapshot.data!;
+          double progress = data.bytesTransferred / data.totalBytes;
+
+          return SizedBox(
+            height: 50,
+            child: Stack(
+              fit: StackFit.expand,
+              children: [
+                LinearProgressIndicator(
+                  value: progress,
+                  backgroundColor: Colors.grey,
+                  color: Colors.green,
+                ),
+                Center(
+                  child: Text(
+                    '${(100 * progress).roundToDouble()}%',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                )
+              ],
+            ),
+          );
+        } else {
+          return const SizedBox(
+            height: 50,
+          );
+        }
+      });
+}
--- flutter_main_page/lib/pages/mainPage/main_page_sub/main_home/main_home.dart
+++ flutter_main_page/lib/pages/mainPage/main_page_sub/main_home/main_home.dart
@@ -1,10 +1,11 @@
 // ignore_for_file: unnecessary_const
 
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter_main_page/main.dart';
-
+import 'package:flutter_main_page/pages/View_pages/image_view.dart';
 import 'package:flutter_main_page/pages/View_pages/notice_view.dart';
 
 import 'package:flutter_main_page/pages/mainPage/main_page_sub/main_community/Community_house/com_community.dart';
@@ -36,56 +37,60 @@ class _MainPage1State extends State<MainPage1> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: ListView(
-      children: [
-        _buildIcon(context, widget.user, widget.isAdmin),
-        const SizedBox(
-          height: 30,
-        ),
-        _buildTop(),
-        const SizedBox(
-          height: 30,
-        ),
-        Container(
-            padding: const EdgeInsets.all(8.0),
-            margin: const EdgeInsets.all(8.0),
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(25),
-                border: Border.all(color: myColor, width: 3)),
-            child: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(left: 8.0),
-                  child: Row(
-                    mainAxisSize: MainAxisSize.max,
-                    children: [
-                      const Expanded(
-                        child: Text(
-                          '공지사항',
-                          style: TextStyle(
-                              fontSize: 25, fontWeight: FontWeight.bold),
+        body: Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ListView(
+        children: [
+          _buildIcon(context, widget.user, widget.isAdmin),
+          Divider(
+            height: 50,
+            color: Colors.black,
+          ),
+          _buildTop(),
+          Divider(
+            height: 50,
+            color: Colors.black,
+          ),
+          Container(
+              padding: const EdgeInsets.all(8.0),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(25),
+                  border: Border.all(color: myColor, width: 3)),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 8.0),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        const Expanded(
+                          child: Text(
+                            '공지사항',
+                            style: TextStyle(
+                                fontSize: 25, fontWeight: FontWeight.bold),
+                          ),
                         ),
-                      ),
-                      TextButton(
-                        onPressed: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) =>
-                                      NoticePage(widget.user, widget.isAdmin)));
-                        },
-                        child: const Text(
-                          '더보기 >',
-                          style: TextStyle(fontSize: 15),
+                        TextButton(
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => NoticePage(
+                                        widget.user, widget.isAdmin)));
+                          },
+                          child: const Text(
+                            '더보기 >',
+                            style: TextStyle(fontSize: 15),
+                          ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                ),
-                _buildBottom(),
-              ],
-            )),
-      ],
+                  _buildBottom(),
+                ],
+              )),
+        ],
+      ),
     ));
   }
 
@@ -116,151 +121,170 @@ class _MainPage1State extends State<MainPage1> {
           }
         });
   }
-}
 
-Widget _buildIcon(BuildContext context, String user, bool? isAdmin) {
-  return Column(
-    children: [
-      const SizedBox(
-        height: 20,
-      ),
-      Row(
-        mainAxisSize: MainAxisSize.max,
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          GestureDetector(
-            onTap: () {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => NoticePage(user, isAdmin)));
-            },
-            child: Column(
-              children: const [
-                Icon(
-                  Icons.notifications,
-                  size: 40,
-                ),
-                Text('공지사항'),
-              ],
-            ),
-          ),
-          GestureDetector(
-            onTap: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => EventPage(user)));
-            },
-            child: Column(
-              children: const [
-                Icon(
-                  Icons.card_giftcard,
-                  size: 40,
-                ),
-                Text('학과이벤트'),
-              ],
-            ),
-          ),
-          GestureDetector(
-            onTap: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => const InfoJobPage()));
-            },
-            child: Column(
-              children: const [
-                Icon(
-                  Icons.lightbulb,
-                  size: 40,
-                ),
-                Text('취업정보'),
-              ],
-            ),
-          ),
-          GestureDetector(
-            onTap: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => ComPage(user)));
-            },
-            child: Column(
-              children: const [
-                Icon(
-                  Icons.reorder,
-                  size: 40,
-                ),
-                Text('익명게시글'),
-              ],
-            ),
-          ),
-        ],
-      ),
-    ],
-  );
-}
-
-Widget _buildTop() {
-  return CarouselSlider(
-    options: CarouselOptions(height: 220.0, autoPlay: true),
-    items: [1, 2, 3, 4, 5].map((i) {
-      return Builder(
-        builder: (BuildContext context) {
-          return Container(
-              // width: MediaQuery.of(context).size.width,
-              margin: const EdgeInsets.symmetric(horizontal: 5.0),
-              decoration: BoxDecoration(
-                  color: Colors.amber,
-                  borderRadius: BorderRadius.circular(8.0)),
-              child: Center(
-                  child: Text(
-                'text $i',
-                style: const TextStyle(fontSize: 16.0),
-              )));
-        },
-      );
-    }).toList(),
-  );
-}
-
-Widget _buildItemNotice(DocumentSnapshot doc, BuildContext context) {
-  final notice =
-      Notice(doc['title'], doc['content'], doc['author'], doc['time']);
-  return Column(
-    children: [
-      ListTile(
-        visualDensity: VisualDensity(horizontal: 0, vertical: -4),
-        onTap: () {
-          Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => NoticeViewPage(notice.title,
-                      notice.content, notice.author, notice.time)));
-        },
-        // leading: const Icon(
-        //   Icons.notifications,
-        //   color: Colors.red,
-        // ),
-        title: Text(
-          "[공지사항] ${notice.title}",
-          style: const TextStyle(
-              color: Colors.deepPurple,
-              fontSize: 17,
-              fontWeight: FontWeight.bold),
+  Widget _buildIcon(BuildContext context, String user, bool? isAdmin) {
+    return Column(
+      children: [
+        const SizedBox(
+          height: 20,
         ),
-        subtitle: Text(
-          "작성자 : ${notice.author}",
-          style: const TextStyle(fontSize: 12),
+        Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => NoticePage(user, isAdmin)));
+              },
+              child: Column(
+                children: const [
+                  Icon(
+                    Icons.notifications,
+                    size: 40,
+                  ),
+                  Text('공지사항'),
+                ],
+              ),
+            ),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => EventPage(user)));
+              },
+              child: Column(
+                children: const [
+                  Icon(
+                    Icons.card_giftcard,
+                    size: 40,
+                  ),
+                  Text('학과이벤트'),
+                ],
+              ),
+            ),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const InfoJobPage()));
+              },
+              child: Column(
+                children: const [
+                  Icon(
+                    Icons.lightbulb,
+                    size: 40,
+                  ),
+                  Text('취업정보'),
+                ],
+              ),
+            ),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => ComPage(user)));
+              },
+              child: Column(
+                children: const [
+                  Icon(
+                    Icons.reorder,
+                    size: 40,
+                  ),
+                  Text('익명게시글'),
+                ],
+              ),
+            ),
+          ],
         ),
-      ),
-    ],
-  );
-}
+      ],
+    );
+  }
 
-Widget _buildNonItem() {
-  return Container(
-    height: 280,
-    child: const Center(
-        child: Text(
-      '아직 등록된 공지가 없습니다.',
-      style: TextStyle(
-          color: Colors.grey, fontSize: 15, fontWeight: FontWeight.bold),
-    )),
-  );
+  Widget _buildTop() {
+    return StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance.collection('eventImage').snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return Center(child: CircularProgressIndicator());
+          }
+          final documents = snapshot.data!.docs;
+
+          return CarouselSlider(
+            options: CarouselOptions(
+              height: 220.0,
+              autoPlay: true,
+              enlargeCenterPage: true,
+            ),
+            items: documents.map((doc) {
+              return Builder(
+                builder: (BuildContext context) {
+                  return Container(
+                      width: MediaQuery.of(context).size.width,
+                      margin: const EdgeInsets.symmetric(horizontal: 5.0),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(8.0),
+                        child: CachedNetworkImage(
+                          imageUrl: doc['url'],
+                          fit: BoxFit.fill,
+                          placeholder: (context, url) => Container(
+                            color: Colors.black,
+                          ),
+                          errorWidget: (context, url, error) =>
+                              Icon(Icons.error),
+                        ),
+                      ));
+                },
+              );
+            }).toList(),
+          );
+        });
+  }
+
+  Widget _buildItemNotice(DocumentSnapshot doc, BuildContext context) {
+    final notice =
+        Notice(doc['title'], doc['content'], doc['author'], doc['time']);
+    return Column(
+      children: [
+        ListTile(
+          visualDensity: VisualDensity(horizontal: 0, vertical: -4),
+          onTap: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => NoticeViewPage(notice.title,
+                        notice.content, notice.author, notice.time)));
+          },
+          // leading: const Icon(
+          //   Icons.notifications,
+          //   color: Colors.red,
+          // ),
+          title: Text(
+            "[공지사항] ${notice.title}",
+            style: const TextStyle(
+                color: Colors.deepPurple,
+                fontSize: 17,
+                fontWeight: FontWeight.bold),
+          ),
+          subtitle: Text(
+            "작성자 : ${notice.author}",
+            style: const TextStyle(fontSize: 12),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildNonItem() {
+    return Container(
+      height: 280,
+      child: const Center(
+          child: Text(
+        '아직 등록된 공지가 없습니다.',
+        style: TextStyle(
+            color: Colors.grey, fontSize: 15, fontWeight: FontWeight.bold),
+      )),
+    );
+  }
 }
